b49d55575c740d3edd87f536604e68cc13e095e1
anderson9813 <Jo465An.d@gmail.com>
2021-03-21 17:35:36 +0400

    0.1mul

---
+++ mulTwoint.py
@@ -0,0 +1,6 @@
+!#/usr/bin/env python3
+x = int(input("Entrez un numero s'il vous plait: "))
+y = int(input("Entrez un deuxieme numero s'il vous plait: "))
+
+z = x * Y
+print("Le resultat de", x , "x", y , "est de", z) 
