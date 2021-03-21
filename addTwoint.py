64b67c3e98784a17b776a5dc2715181dc9a81e45
anderson9813 <Jo465An.d@gmail.com>
2021-03-21 14:38:27 +0400

    0.2add

--- addTwoint.py
+++ addTwoint.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-x = int(input("Entrez le premier nombre s'il vous plait: "))
-y = int(input("Entrez le deuxieme nombre s'il vous plait: "))
+try:
+     x = int(input("Entrez le premier nombre s'il vous plait: "))
+     y = int(input("Entrez le deuxieme nombre s'il vous plait: "))
 
-z = x + y 
-print("Le resultat de", x , "+", y , "est de", z)
+     z = x + y 
+     print("Le resultat de", x , "+", y , "est de", z)
+except:
+       print("Il y a une erreur")
