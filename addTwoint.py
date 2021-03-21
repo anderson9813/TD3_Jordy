0754dcc123e9d56afc00fe057613f0e2385d620a
anderson9813 <Jo465An.d@gmail.com>
2021-03-21 17:26:25 +0400

    0.3add

--- addTwoint.py
+++ addTwoint.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-try:
-     x = int(input("Entrez le premier nombre s'il vous plait: "))
-     y = int(input("Entrez le deuxieme nombre s'il vous plait: "))
 
-     z = x + y 
-     print("Le resultat de", x , "+", y , "est de", z)
-except:
-       print("Il y a une erreur")
+def add(x, y):
+     output = x + y
+     return output
+x = input("Entrez le premier nombre s'il vous plait: ")
+while isinstance(x, str):
+    try:
+         x = int(x)
+    except:
+         x = input("vous n'avez pas entre de numero.\nEntrez le premier nombre")
+print()
+y =input("Entrez le deuxiem numero s'il vous plait: ")
+while isinstance(y, str):
+    try:
+         y = int(y)
+    except:
+         y = input("Vous n'avez pas entrer de numero\n entrer le deuxieme nombre")
+print(add(x, y))
